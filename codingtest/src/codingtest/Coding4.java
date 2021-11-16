468c9a62c66deadb22a42ee3b676a4bce80727ec
jeongdahyeon <jeongdahyeon@172.30.1.10>
2021-11-17 08:31:34 +0900

    4까지 코딩 혼자공부 완성(4/104)

--- codingtest/src/codingtest/Coding4.java
+++ codingtest/src/codingtest/Coding4.java
@@ -1,13 +1,19 @@
 package codingtest;
 
+import java.util.Scanner;
+
 public class Coding4 {
 
-	
-	
-	
 	public static void main(String[] args) {
 		
+	Scanner scanner = new Scanner(System.in);
+	int inputCount = scanner.nextInt();
+	String [] arrayInput = new String[inputCount];
+	for(int i = 0;i< inputCount;i++) {
+		arrayInput[i] = scanner.next();
+		StringBuffer sb = new StringBuffer(arrayInput[i]);
+	String answer=	sb.reverse().toString();
+	System.out.println(answer);
 	}
-	
-	
+	}
 }
